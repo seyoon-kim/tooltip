5967578c02d823cac33e777207cd426edfa900f6
seyoon-kim <seyoon.kim@nhnent.com>
2017-09-14 16:42:01 +0900

    Update, .toolTip class를 가진 엘리멘트들이 각각 toolTip Class가 제대로 추가 되었는지 확인
    init함수의 인자 배열이 여러개의 객체를 받는 경우 각각의 ToolTip객체를 생성하기 위해 ToolTip 생성자를 생성하여 각각의 객체를 생성할 수 있도록 하였다.

--- test/spec.js
+++ test/spec.js
@@ -1,21 +1,41 @@
+var ToolTip = function(eleToolTip) {
+    var selectorName = eleToolTip.element
+    var eleSelectors = Domutil.querySelectorAll(selectorName);
+
+    for (var i = 0; i < eleSelectors.length; i += 1) {
+        Domclass.addClass(eleSelectors[i], "toolTip");
+    }
+
+}
+
 var mytooltip = {
     init : function (arrEle){
-        var eleFirst = Domutil.querySelectorAll(arrEle[0].element)[0];
-        Domclass.addClass(eleFirst, "toolTip");
+        for (var i = 0; i < arrEle.length; i += 1) {
+            new ToolTip(arrEle[i]);
+        }
     }
 }
 
 describe("mytooltip.init", function() {
-  beforeEach(function() {
-    document.body.innerHTML = '<p>test... <strong id="first">consectetur</strong>';
-  });
+    beforeEach(function() {
+        document.body.innerHTML = '<p>test... <strong id="first">consectetur</strong> blar... blar... blar... <strong class="second">ullamco</strong>blar... <strong class="second">ullamco 0202</strong>';
+        mytooltip.init([{
+            element: "#first"
+        }, {
+            element: ".second"
+        }]);
+    });
 
-  it("should be have .toolTip", function() {
-    mytooltip.init([{
-      element: "#first"
-    }]);
+    // #first ID를 가진 엘리멘트에 toolTip Class가 제대로 추가 되었는지 확인하기.
+    it("should be have .toolTip", function() {
+        var eleFirst = Domutil.querySelectorAll("#first")[0];
+        expect(Domclass.hasClass(eleFirst, "toolTip")).toBe(true);
+    });
 
-    var eleFirst = Domutil.querySelectorAll("#first")[0];
-    expect(Domclass.hasClass(eleFirst, "toolTip")).toBe(true);
-  });
+    // .toolTip class를 가진 엘리멘트들이 각각 toolTip Class가 제대로 추가 되었는지 확인하기.
+    fit("should be have each .toolTip class", function() {
+        var eleSecond = Domutil.querySelectorAll(".second");
+        expect(Domclass.hasClass(eleSecond[0], "toolTip")).toBe(true);
+        expect(Domclass.hasClass(eleSecond[1], "toolTip")).toBe(true);
+    });
 });
