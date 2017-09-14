1fe1ed07c5272cdbab1ee8f31d2b5fa49c687e98
seyoon-kim <seyoon.kim@nhnent.com>
2017-09-14 16:13:56 +0900

    Update, init함수 해당 엘리멘트 tooltip 클래스 추가 확인
    init함수에서 초기화 하고자하는 배열을 인자로 받는다. 배열로 담긴 엘리멘트 중 element 속성으로 찾아진 엘리멘트에 tooltip 클래스가 제대로 추가되었는지 확인한다.

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,21 @@
-describe("test", function() {  
-  it("test", function() {
-    expect(true).toBe(true);
+var mytooltip = {
+    init : function (arrEle){
+        var eleFirst = Domutil.querySelectorAll(arrEle[0].element)[0];
+        Domclass.addClass(eleFirst, "toolTip");
+    }
+}
+
+describe("mytooltip.init", function() {
+  beforeEach(function() {
+    document.body.innerHTML = '<p>test... <strong id="first">consectetur</strong>';
+  });
+
+  it("should be have .toolTip", function() {
+    mytooltip.init([{
+      element: "#first"
+    }]);
+
+    var eleFirst = Domutil.querySelectorAll("#first")[0];
+    expect(Domclass.hasClass(eleFirst, "toolTip")).toBe(true);
   });
 });
