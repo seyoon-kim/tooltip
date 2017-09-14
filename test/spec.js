abf154b637a5794e0e6a4dd93809a60e64df77e3
seyoon-kim <seyoon.kim@nhnent.com>
2017-09-14 21:13:45 +0900

    Update, mytooltip.edit함수 추가
    mytooltip.edit 함수를 통해 #first ID를 가진 요소의 toolTipText가 변경되었는지 확인

--- test/spec.js
+++ test/spec.js
@@ -1,75 +1,93 @@
-var ToolTip = function(eleToolTip) {
-    this.delay = eleToolTip.delay || 0; // delay 값이 없다면 0으로 초기화
 
-    var selectorName = eleToolTip.element;
-    var contents = eleToolTip.contents;
-    var eleSelectors = Domutil.querySelectorAll(selectorName);
-    var eleToolTipText = null;
-
-    for (var i = 0; i < eleSelectors.length; i += 1) {
-        Domclass.addClass(eleSelectors[i], "toolTip");
-        eleToolTipText = document.createElement("DIV");
-        Domclass.addClass(eleToolTipText, "toolTipText");
-        eleToolTipText.innerHTML = contents;
-        eleSelectors[i].appendChild(eleToolTipText);
-
-        this.addHandlerMouseoverToolTip(eleSelectors[i]);
-        this.addHandlerMouseoutToolTip(eleSelectors[i]);
-    }
-
-}
-
-ToolTip.prototype.addHandlerMouseoverToolTip = function(ele) {
-    var that = this;
-    Eventutil.addHandler(ele, "mouseover", function(e) {
-        var target = e.target;
-        that.mouseoverToolTip(target);
-    });
-}
-
-ToolTip.prototype.mouseoverToolTip = function(target){
-    var eleToolTipText;
-    if (Domclass.hasClass(target, "toolTipText")) {
-        Domclass.addClass(target, "on");
-        return;
-    }
-    eleToolTipText = target.querySelector(".toolTipText");
-
-    // delay가 존재하는 경우 해당 시간이 흐른뒤 클래스 추가
-    if (this.delay === 0){
-        Domclass.addClass(eleToolTipText, "on");
-    }else{
-        setTimeout(function(){
-            Domclass.addClass(eleToolTipText, "on");
-        }, this.delay);
-    }
-}
-
-ToolTip.prototype.addHandlerMouseoutToolTip = function(ele) {
-    var that = this;
-    Eventutil.addHandler(ele, "mouseout", function(e) {
-        var target = e.target;
-        that.mouseoutToolTip(target);
-    });
-}
-
-ToolTip.prototype.mouseoutToolTip = function(target){
-    var eleToolTipText;
-    if (Domclass.hasClass(target, "toolTipText")) {
-        Domclass.removeClass(target, "on");
-        return;
-    }
-    eleToolTipText = target.querySelector(".toolTipText");
-    Domclass.removeClass(eleToolTipText, "on");
-}
 
 
 var mytooltip = (function() {
     var arrToolTip = [];
 
+    var ToolTip = function(eleToolTip) {
+        this.delay = eleToolTip.delay || 0; // delay 값이 없다면 0으로 초기화
+        this.selectorName = eleToolTip.element;
+        this.contents = eleToolTip.contents;
+
+        this.eleSelectors = Domutil.querySelectorAll(this.selectorName);
+        var eleToolTipText = null;
+
+        for (var i = 0; i < this.eleSelectors.length; i += 1) {
+            Domclass.addClass(this.eleSelectors[i], "toolTip");
+            eleToolTipText = document.createElement("DIV");
+            Domclass.addClass(eleToolTipText, "toolTipText");
+            eleToolTipText.innerHTML = this.contents;
+            this.eleSelectors[i].appendChild(eleToolTipText);
+
+            this.addHandlerMouseoverToolTip(this.eleSelectors[i]);
+            this.addHandlerMouseoutToolTip(this.eleSelectors[i]);
+        }
+
+    }
+
+    ToolTip.prototype.addHandlerMouseoverToolTip = function(ele) {
+        var that = this;
+        Eventutil.addHandler(ele, "mouseover", function(e) {
+            var target = e.target;
+            that.mouseoverToolTip(target);
+        });
+    }
+
+    ToolTip.prototype.mouseoverToolTip = function(target){
+        var eleToolTipText;
+        if (Domclass.hasClass(target, "toolTipText")) {
+            Domclass.addClass(target, "on");
+            return;
+        }
+        eleToolTipText = target.querySelector(".toolTipText");
+
+        // delay가 존재하는 경우 해당 시간이 흐른뒤 클래스 추가
+        if (this.delay === 0){
+            Domclass.addClass(eleToolTipText, "on");
+        }else{
+            setTimeout(function(){
+                Domclass.addClass(eleToolTipText, "on");
+            }, this.delay);
+        }
+    }
+
+    ToolTip.prototype.addHandlerMouseoutToolTip = function(ele) {
+        var that = this;
+        Eventutil.addHandler(ele, "mouseout", function(e) {
+            var target = e.target;
+            that.mouseoutToolTip(target);
+        });
+    }
+
+    ToolTip.prototype.mouseoutToolTip = function(target){
+        var eleToolTipText;
+        if (Domclass.hasClass(target, "toolTipText")) {
+            Domclass.removeClass(target, "on");
+            return;
+        }
+        eleToolTipText = target.querySelector(".toolTipText");
+        Domclass.removeClass(eleToolTipText, "on");
+    }
+
+    ToolTip.prototype.editInfo = function(objInfo) {
+        this.contents = objInfo.contents;
+        this.delay = objInfo.delay;
+        for(var i = 0; i < this.eleSelectors.length; i += 1) {
+            this.eleSelectors[i].childNodes[1].innerText = this.contents;
+        }
+    }
+
     var init = function (arrEle){
         for (var i = 0; i < arrEle.length; i += 1) {
             arrToolTip.push(new ToolTip(arrEle[i]));
+        }
+    }
+
+    var edit = function(selector, objInfo) {
+        for(var i = 0; i < arrToolTip.length; i += 1) {
+            if (arrToolTip[i].selectorName === selector) {
+                arrToolTip[i].editInfo(objInfo);
+            }
         }
     }
 
@@ -79,6 +97,7 @@ var mytooltip = (function() {
 
     return {
         init : init,
+        edit : edit,
         getArrToolTip : getArrToolTip
     }
 
@@ -185,5 +204,30 @@ describe("mytooltip.init", function() {
             expect(Domclass.hasClass(eleToolTipText, "on")).toBe(true);
         }, 500);
         jasmine.clock().uninstall();
+    });
+});
+
+describe("mytooltip.edit", function() {
+    beforeEach(function() {
+        document.body.innerHTML = '<p>test... <strong id="first">consectetur</strong> blar... blar... blar... <strong class="second">ullamco</strong>blar... <strong class="second">ullamco 0202</strong>';
+        mytooltip.init([{
+            element: "#first",
+            contents: 'Duis aute irure dolor',
+            delay: 500
+        }, {
+            element: ".second",
+            contents: 'labore et dolore magna aliqua'
+        }]);
+
+        mytooltip.edit('#first', {
+            contents: 'Lorem ipsum',
+            delay: 0
+        });
+    });
+
+    // #first ID를 가진 요소의 toolTipText가 변경되었는지 확인
+    fit("should be change toolTipText", function() {
+        var eleToolTipText = Domutil.querySelector("#first .toolTipText")[0];
+        expect(eleToolTipText.innerText).toEqual('Lorem ipsum');
     });
 });
