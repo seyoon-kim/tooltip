80251a5e76f6f32d7f3411c506833d5a96983648
seyoon-kim <seyoon.kim@nhnent.com>
2017-09-14 17:09:12 +0900

    Update, .toolTip 요소안에 .toolTipText 요소가 제대로 생겼는지 확인하기
    .toolTip요소안에 .toolTipText 클래스를 가진 요소를 만들어 옵션값으로 받는 contents의 text 문자를 가지도록 한다.

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,13 @@
 var ToolTip = function(eleToolTip) {
-    var selectorName = eleToolTip.element
+    var selectorName = eleToolTip.element;
     var eleSelectors = Domutil.querySelectorAll(selectorName);
+    var eleToolTipText = null;
 
     for (var i = 0; i < eleSelectors.length; i += 1) {
         Domclass.addClass(eleSelectors[i], "toolTip");
+        eleToolTipText = document.createElement("DIV");
+        Domclass.addClass(eleToolTipText, "toolTipText");
+        eleSelectors[i].appendChild(eleToolTipText);
     }
 
 }
@@ -33,9 +37,19 @@ describe("mytooltip.init", function() {
     });
 
     // .toolTip class를 가진 엘리멘트들이 각각 toolTip Class가 제대로 추가 되었는지 확인하기.
-    fit("should be have each .toolTip class", function() {
+    it("should be have each .toolTip class", function() {
         var eleSecond = Domutil.querySelectorAll(".second");
         expect(Domclass.hasClass(eleSecond[0], "toolTip")).toBe(true);
         expect(Domclass.hasClass(eleSecond[1], "toolTip")).toBe(true);
+    });
+
+    // .toolTip 요소안에 .toolTipText 요소가 제대로 생겼는지 확인하기
+    it("should be have each .toolTip class", function() {
+        var eleToolTipText = Domutil.querySelectorAll(".toolTip .toolTipText");
+        var i = 0;
+        var eleToolTipTextLength = eleToolTipText.length;
+        for (; i < eleToolTipTextLength; i += 1) {
+            expect(Domclass.hasClass(eleToolTipText[i], "toolTipText")).toBe(true);
+        }
     });
 });
